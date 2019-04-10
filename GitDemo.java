283b62a22d56ebe6d13fc6fe9c12d7056349cfdd
Cheng Yu <1951658364@qq.com>
2019-04-10 21:05:09 +0800

    add test2

--- GitDemo.java
+++ GitDemo.java
@@ -2,5 +2,10 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("Git");
 		System.out.println("Test");
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
 	}
 }
