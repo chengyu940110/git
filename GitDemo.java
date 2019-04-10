58524c06aab999f144807c4fe0c660b11848da8f
Cheng Yu <1951658364@qq.com>
2019-04-10 21:15:48 +0800

    add stash

---
+++ GitDemo.java
@@ -0,0 +1,11 @@
+public class GitDemo {
+	public static void main(String[] args) {
+		System.out.println("Git");
+		System.out.println("Test");
+		System.out.println("Test1");
+		System.out.println("D7");
+		System.out.println("Test5");
+		System.out.println("Test");
+		System.out.println("Stash");
+	}
+}
